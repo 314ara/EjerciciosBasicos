fc0efd012176b7d32e1e3ad4abfe574034c484c9
aracelly <314aracelly@gmail.com>
2023-06-16 16:08:40 -0400

    ejercicio3

--- objetos/ejercicio3.js
+++ objetos/ejercicio3.js
@@ -1,3 +1,12 @@
 /*Define un objeto, que tenga dos propiedades: precio y descuento y un método neto. 
 El método calculará el precio con el descuento aplicado. Mostrar por consola. Por ejemplo objeto vestido,
  precio 400 y descuento 10. El método devolverá como resultado 360 (400 - 10*400/100).*/
+ const telescopio = {
+    precio: 500,
+    descuento: 20,
+    neto: function() {
+      return this.precio - (this.descuento * this.precio / 100);
+    }
+  };
+  
+  console.log(telescopio.neto());
