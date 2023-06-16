a2b209774c21f253e9931b80eb72a387333bc36c
aracelly <314aracelly@gmail.com>
2023-06-16 16:02:48 -0400

    ejercicio2 concluido

--- objetos/ejercicio2.js
+++ objetos/ejercicio2.js
@@ -3,3 +3,18 @@
  en cada una de ellas. El script imprimirá el nombre y la media(promedio) de sus calificaciones.
  Por ejemplo, guardar las calificaciones de un alumno de nombre Juan, Inglés: 9, programación: 8,
   HTML: 7. Sacará Nota media de Juan 8*/
+  const alumno = {
+    nombre: 'Celula',
+    edad: 7,
+    materia : {
+        lenguaje: 7,
+        frances: 5,
+        actuacion: 6,
+    },
+    promedioNotas: function (){
+        const promedio = ((this.materia.lenguaje + this.materia.frances + this.materia.actuacion) / 3);
+        console.log(`El promedio de ${this.nombre} es ${promedio}`);
+    }
+}
+
+alumno.promedioNotas();
